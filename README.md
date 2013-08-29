d924bc2952659f1e5ec81a146132eaeaff1fe2ea
Yasutaka ATARASHI <yakex@cpan.org>
2013-08-29 22:03:09 +0900

    Initial commit.

---
+++ README.md
@@ -0,0 +1,85 @@
+# NAME
+
+AnyEvent::Digest - A tiny AnyEvent wrapper for Digest::\*
+
+# VERSION
+
+version v0.0.0
+
+# SYNOPSIS
+
+    use AnyEvent::Digest;
+    use Digest::SHA;
+    my $ctx = AnyEvent::Digest->new('SHA', opts => [1], unit => 65536, backend => 'IO::AIO');
+    # In addition that $ctx can be used as Digest::* object, you can call add*_async()
+    $ctx->addfile_async($file)->recv(sub {
+      # Do something like the followings
+      my $ctx = $_[0]->recv;
+      print $ctx->hexdigest;
+    });
+
+# DESCRIPTION
+
+To calculate message digest for large files may take several seconds.
+It may block your program even if you use [AnyEvent](http://search.cpan.org/perldoc?AnyEvent).
+This module is a tiny AnyEvent wrapper for Digest::\* modules,
+not to block your program during digest calculation.
+
+# METHODS
+
+In addition to the following methods, other methods are forwarded to the base module.
+
+## `new($base, %args)`
+
+This is a constructor method.
+`$base` specifies a module name for base implementation, which is expected to be one of `Digest::*` modules.
+Available keys of `%args` are as follows:
+
+- `opts`
+
+    passed to `$base::new` as `@{$args{opts}}`. It must be an array reference.
+
+- `unit`
+
+    specifies an amount of one time read for addfile(). Not yet implemented.
+
+- `backend`
+
+    specifies a backend module to handle asynchronous read. Not yet implemented.
+
+## `add_async(@dat)`
+
+Each item in `@dat` are added by `add($dat)`.
+Between the adjacent `add()`, other AnyEvent watchers have chances to run.
+It returns a condition variable receiving this object itself.
+
+## `addfile_async($filename)`
+
+## `addfile_async(*handle)`
+
+`add()` is called repeatedly read from `$filename` or `*handle` by the specified unit.
+Between the adjacent `add()`, other AnyEvent watchers have chances to run.
+It returns a condition variable receiving this object itself.
+
+## `add_bits_async()`
+
+Same as `add_bits()`, except it returns a condition variable receiving this object itself.
+
+## `addfile()`
+
+This method uses blocking wait + `addfile_async()`.
+
+## `addfile_base()`
+
+Forwarded to `addfile()` in the base module.
+
+# AUTHOR
+
+Yasutaka ATARASHI <yakex@cpan.org>
+
+# COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2013 by Yasutaka ATARASHI.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
---
+++ lib/AnyEvent/Digest.pm
@@ -0,0 +1,165 @@
+package AnyEvent::Digest;
+
+use strict;
+use warnings;
+
+# ABSTRACT: A tiny AnyEvent wrapper for Digest::*
+# VERSION
+
+use AnyEvent;
+use Scalar::Util qw(refaddr);
+
+# Most methods are falled back to Digest
+our $AUTOLOAD;
+sub AUTOLOAD
+{
+    my $self = shift;
+    my $called = $AUTOLOAD =~ s/.*:://r;
+    $self->{base}->$called(@_);
+}
+
+sub DESTROY {}
+
+sub _by_idle
+{
+    my ($self, $cv, $work) = @_;
+    my $call; $call = sub {
+        if($work->()) {
+            my $w; $w = AE::idle sub {
+                undef $w;
+                $call->();
+            };
+        } else {
+            $cv->send($self);
+        }
+    };
+    $call->();
+}
+
+sub new
+{
+    my ($class, $base, %args) = @_;
+    $class = ref $class || $class;
+    $args{unit} ||= 65536;
+    return bless {
+        base => $base->new(@{$args{opts}}),
+        map { $_, $args{$_} } qw(backend unit),
+    }, $class;
+}
+
+sub add_async
+{
+    my $self = shift;
+    my $cv = AE::cv;
+    my (@dat) = @_;
+    $self->_by_idle($cv, sub {
+        my $dat = shift @dat;
+        $self->{base}->add($dat);
+        return scalar @dat;
+    });
+    return $cv;
+}
+
+sub addfile_async
+{
+    my ($self, $target, $mode) = @_;
+    my $cv = AE::cv;
+    my $fh;
+    if(ref $target) {
+        $fh = $target;
+    } else {
+        open $fh, '<:raw', $target;
+    }
+    $self->_by_idle($cv, sub {
+        read $fh, my $dat, $self->{unit};
+        $self->{base}->add($dat);
+        close $fh if eof $fh;
+        return ! eof $fh;
+    });
+    return $cv;
+}
+sub addfile
+{
+    return shift->addfile_async(@_)->recv;
+}
+sub addfile_base
+{
+    return shift->{base}->addfile(@_);
+}
+
+sub add_bits_async
+{
+    my $self = shift;
+    my $cv = AE::cv;
+    $self->{base}->add_bits(@_);
+    $cv->send($self);
+    return $cv;
+}
+
+1;
+__END__
+
+=head1 SYNOPSIS
+
+  use AnyEvent::Digest;
+  use Digest::SHA;
+  my $ctx = AnyEvent::Digest->new('SHA', opts => [1], unit => 65536, backend => 'IO::AIO');
+  # In addition that $ctx can be used as Digest::* object, you can call add*_async()
+  $ctx->addfile_async($file)->recv(sub {
+    # Do something like the followings
+    my $ctx = $_[0]->recv;
+    print $ctx->hexdigest;
+  });
+
+=head1 DESCRIPTION
+
+To calculate message digest for large files may take several seconds.
+It may block your program even if you use L<AnyEvent>.
+This module is a tiny AnyEvent wrapper for Digest::* modules,
+not to block your program during digest calculation.
+
+=head1 METHODS
+
+In addition to the following methods, other methods are forwarded to the base module.
+
+=method C<new($base, %args)>
+
+This is a constructor method.
+C<$base> specifies a module name for base implementation, which is expected to be one of C<Digest::*> modules.
+Available keys of C<%args> are as follows:
+
+=for :list
+= C<opts>
+passed to C<$base::new> as C<@{$args{opts}}>. It must be an array reference.
+= C<unit>
+specifies an amount of one time read for addfile(). Not yet implemented.
+= C<backend>
+specifies a backend module to handle asynchronous read. Not yet implemented.
+
+=method C<add_async(@dat)>
+
+Each item in C<@dat> are added by C<add($dat)>.
+Between the adjacent C<add()>, other AnyEvent watchers have chances to run.
+It returns a condition variable receiving this object itself.
+
+=method C<addfile_async($filename)>
+
+=method C<addfile_async(*handle)>
+
+C<add()> is called repeatedly read from C<$filename> or C<*handle> by the specified unit.
+Between the adjacent C<add()>, other AnyEvent watchers have chances to run.
+It returns a condition variable receiving this object itself.
+
+=method C<add_bits_async()>
+
+Same as C<add_bits()>, except it returns a condition variable receiving this object itself.
+
+=method C<addfile()>
+
+This method uses blocking wait + C<addfile_async()>.
+
+=method C<addfile_base()>
+
+Forwarded to C<addfile()> in the base module.
+
+=cut
